91c22d1d4fadf8723021f84edda8c5b02c777fd7
Elaulas Cristiano <elaulas@gmail.com>
2018-06-08 21:45:51 -0500

    Jaws Update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
-var mysql = require('mysql');
-var connection; 
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
 
+//Heroku Connection Object
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -14,9 +16,15 @@ if(process.env.JAWSDB_URL) {
 });
 
 };
-connection.connect(function(err){
-    if(err)throw err;
 
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
